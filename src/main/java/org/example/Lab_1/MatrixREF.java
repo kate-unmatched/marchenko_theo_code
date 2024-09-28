797f4e515058d44e70b4ab0a56e15669122450ec
kate_killer <kate_unmatched@mail.ru>
2024-09-28 21:37:08 +0400

    Lab 1

--- src/main/java/org/example/Lab_1/MatrixREF.java
+++ src/main/java/org/example/Lab_1/MatrixREF.java
@@ -5,18 +5,35 @@ import org.apache.commons.math3.linear.*;
 public class MatrixREF {
 
     public static void main(String[] args) {
-        int[][] matrix = {
-                {1, 0, 1, 0, 0, 0},
-                {0, 1, 1, 0, 1, 1},
-                {0, 0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 1, 1},
-                {0, 0, 0, 0, 0, 1}
+        System.out.println("\n--- TASK 1.1 and 1.2 ---");
+
+//        int[][] originMatrix = {
+//                {1, 0, 1, 0, 0, 0},
+//                {0, 1, 1, 0, 1, 1},
+//                {0, 0, 0, 1, 0, 0},
+//                {0, 0, 0, 0, 1, 1},
+//                {0, 0, 0, 0, 0, 1}
+//        };
+
+        int[][] originMatrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
         };
 
-        int[][] refMatrix = ref(matrix);
 
-        System.out.println("REF matrix:");
+        System.out.println("\nOriginal Matrix:");
+        printMatrix(originMatrix);
+
+        int[][] refMatrix = ref(originMatrix);
+
+        System.out.println("REF Matrix:");
         printMatrix(refMatrix);
+
+        int[][] rrefMatrix = rref(originMatrix);
+
+        System.out.println("RREF Matrix:");
+        printMatrix(rrefMatrix);
     }
 
     public static int[][] ref(int[][] matrix) {
@@ -37,10 +54,12 @@ public class MatrixREF {
                 }
             }
 
+            // Меняем строки
             int[] temp = matrix[i];
             matrix[i] = matrix[r];
             matrix[r] = temp;
 
+            // Нормализуем ведущий элемент
             int leadValue = matrix[r][lead];
             if (leadValue != 0) {
                 for (int j = 0; j < cols; j++) {
@@ -48,15 +67,81 @@ public class MatrixREF {
                 }
             }
 
-            for (i = 0; i < rows; i++) {
-                if (i != r && matrix[i][lead] != 0) {
+            // Обрабатываем строки ниже текущей
+            for (i = r + 1; i < rows; i++) {
+                if (matrix[i][lead] != 0) {
+                    int factor = matrix[i][lead];
                     for (int j = 0; j < cols; j++) {
-                        matrix[i][j] ^= matrix[r][j];
+                        matrix[i][j] ^= matrix[r][j] * factor;
                     }
                 }
             }
+
             lead++;
         }
+        return matrix;
+    }
+
+    public static int[][] rref(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int lead = 0;
+        for (int r = 0; r < rows; r++) {
+            if (cols <= lead) return matrix;
+
+            int i = r;
+            while (matrix[i][lead] == 0) {
+                i++;
+                if (rows == i) {
+                    i = r;
+                    lead++;
+                    if (cols == lead) return matrix;
+                }
+            }
+
+            // Меняем строки
+            int[] temp = matrix[i];
+            matrix[i] = matrix[r];
+            matrix[r] = temp;
+
+            // Нормализуем ведущий элемент
+            if (matrix[r][lead] != 0) {
+                for (int j = 0; j < cols; j++) {
+                    matrix[r][j] /= matrix[r][lead];
+                }
+            }
+
+            // Зануляем элементы ниже текущей строки
+            for (i = 0; i < rows; i++) {
+                if (i != r && matrix[i][lead] != 0) {
+                    int factor = matrix[i][lead];
+                    for (int j = 0; j < cols; j++) {
+                        matrix[i][j] ^= matrix[r][j] * factor;
+                    }
+                }
+            }
+
+            lead++;
+        }
+
+        // Дополнительный шаг: зануляем элементы над ведущими
+        for (int r = rows - 1; r >= 0; r--) {
+            for (int leadCol = 0; leadCol < cols; leadCol++) {
+                if (matrix[r][leadCol] == 1) {
+                    for (int i = 0; i < r; i++) {
+                        if (matrix[i][leadCol] != 0) {
+                            int factor = matrix[i][leadCol];
+                            for (int j = 0; j < cols; j++) {
+                                matrix[i][j] ^= matrix[r][j] * factor;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
         return matrix;
     }
 
@@ -67,6 +152,7 @@ public class MatrixREF {
             }
             System.out.println();
         }
+        System.out.println();
     }
 }
 
